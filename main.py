b6e1e4fe785d4ba7fb6c4fd79a5c19a40bb29df3
CaidenHong <75390871+CaidenHong@users.noreply.github.com>
2021-01-25 09:11:55 -0500

    Finished COde except for Animation

--- main.py
+++ main.py
@@ -3,49 +3,226 @@ A basic pygame template
 """
  
 import pygame
- 
+import time
+import random
+
+pygame.init() 
+
 # Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
- 
-pygame.init()
-  
+BLACK      = (   0,   0,   0)
+WHITE      = ( 255, 255, 255)
+GREEN      = (   0, 255,   0)
+DARK_GREEN = (   6, 107,   6)
+RED        = ( 255,   0,   0)
+GREY       = (  82,  82,  82)
+
 # Set the width and height of the screen [width, height]
-size = (700, 500)
-screen = pygame.display.set_mode(size)
+screen_width = 700
+screen_height = 500
+
+screen = pygame.display.set_mode((screen_width, screen_height))
  
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Caiden's Snake Game")
  
+#Load Images
+
+#Positions of Images
+
 #Loop until the user clicks the close button.
-done = False
- 
+
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
+# Defining Variables
+
+head_x = 350
+head_y = 250
+head_x_velocity = 0
+head_y_velocity = 0
+head_width = 15
+head_height = 15
+
+intro_x = 1
+intro_y = 1
+intro_x_velocity = 5
+intro_y_velocity = 5
+intro_width = 700
+intro_height = 500
+intro_go = False
+
+body = 10
+snake_speed = 15
+
+paragraph_x = 4
+paragraph_y = 4
+paragraph_x_velocity = 5
+paragraph_y_velocity = 5
+paragraph_go = False
+
+font_style = pygame.font.SysFont(None, 40)
+font = pygame.font.SysFont(None, 40)
+
+game_over_sfx = pygame.mixer.Sound("retro_game_over.wav")
+food_sfx = pygame.mixer.Sound("human_eating.wav")
+boop_sfx = pygame.mixer.Sound("boop.wav")
+game_winner = pygame.mixer.Sound("game_winner.wav")
+
+score_font = pygame.font.SysFont("comicsansms", 30)
+
+def user_score(score):
+    value = score_font.render("Score:  " + str(score), True, WHITE)
+    screen.blit(value, [4, 0])
+
+def our_snake(body, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, GREEN, [x[0], x[1], body, body])
+
+def message(msg, colour):
+    mesg = font_style.render(msg, True, colour)
+    screen.blit(mesg, [80, 230])
+
+def paragraph(msg, colour):
+    paragraph_y = 4
+    paragraph_velocity = 5
+    paragraph_go = False
+    msg_list = msg.split("\n")
+    for line in msg_list:
+        mesg = font.render(line, True, colour)
+        screen.blit(mesg, [paragraph_x,paragraph_y])
+        paragraph_y += 25
+                
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    head_x = screen_width / 2
+    head_y = screen_height / 2
+    
+    head_x_velocity = 0
+    head_y_velocity = 0
+
+    snake_list = []
+    snake_length = 1
+
+    food_x = round(random.randrange(0, screen_width - body) / 10.0) * 10.0
+    food_y = round(random.randrange(0, screen_height - body) / 10.0) * 10.0
+
 # -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-  
-    # --- Game logic should go here
- 
-    # --- Drawing code should go here
-     
+    while not game_over:
+
+        # --- Main event loop
+        while game_close == True:
+            screen.fill(RED)
+            message("Game Over! Press 1-Quit or 2-Play Again", BLACK)
+            user_score(snake_length - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_2:
+                        boop_sfx.play()
+                        gameLoop()
+        
+        for event in pygame.event.get(): # User did something      
+            if event.type == pygame.QUIT:
+                print("User asked to quit.")
+                game_over = True 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    head_x_velocity = -body
+                    head_y_velocity = 0
+                elif event.key == pygame.K_RIGHT:
+                    head_x_velocity = body
+                    head_y_velocity = 0
+                elif event.key == pygame.K_DOWN:
+                    head_x_velocity = 0
+                    head_y_velocity = body
+                elif event.key == pygame.K_UP:
+                    head_x_velocity = 0
+                    head_y_velocity = -body
+                elif event.key == pygame.K_SPACE:
+                    intro_go = not intro_go
+                    paragraph_go = not paragraph_go
+
+            if intro_go:
+                intro_x += intro_x_velocity
+
+            if paragraph_go:
+                paragraph_x += paragraph_x_velocity
+            
+
+    #Game over if snake hits wall   
+        if head_x >= screen_width or head_x < 0 or head_y >= screen_height or head_y < 0:
+            game_over_sfx.play()    
+            game_close = True
+            
+        head_x += head_x_velocity
+        head_y += head_y_velocity
+
+        if (snake_length - 1) >= 20 and game_close == False:
+            font = pygame.font.SysFont('Oswald',15, True, False)
+            text = font.render("Congratulations! You have beaten the antivirus snake! Keep Going!")
+            screen.blite(text, [30, 30])
+            game_winner.play()
+
+    # Flag that we are game_over so we exit this loop
     # First, clear the screen to white or whatever background colour. 
     # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
-     
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
+        screen.fill(GREY)
+
+    # --- Game logic should go here
+
+    # --- Drawing code should go here
+        #Food
+        pygame.draw.rect(screen, RED, [food_x, food_y, body, body])
+
+        #Snake Head
+        
+        snake_head = []
+        snake_head.append(head_x)
+        snake_head.append(head_y)
+        snake_list.append(snake_head)
+
+        if len(snake_list) > snake_length:
+            del snake_list[0]
+
+        for x in snake_list[:-1]:
+            if x == snake_head:
+                game_over_sfx.play()
+                game_close = True
+
+        our_snake(body, snake_list)
+        user_score(snake_length - 1)
+
+        
+
+        if head_x == food_x and head_y == food_y:
+            food_sfx.play()
+            food_x = round(random.randrange(0, screen_width - body) / 10.0) * 10.0
+            food_y = round(random.randrange(0, screen_height - body) / 10.0) * 10.0
+
+            snake_length += 1
+
+        #Introduction
+        pygame.draw.rect(screen, WHITE, [intro_x, intro_y, intro_width, intro_height])
+        
+
+        
+        text = "Welcome to the Antivirus Snake! A computer has\nbeen infected with malware and you have inserted\nthe Anti Virus Snake into the computer. To get rid\nof the malware, you must achieve a score of 20 or\ngreater in order for the antivirus snake to become\nsuccesful. If you reach a score past 20, keep going\nas the Anti Virus Snake will install antivirus and\nmalware protection programs. The higher the\nscore, the more protected the computer is from\nviruses. \n \nUse the arrow keys to move and press Space Bar\nto dismiss this screen."
+
+        paragraph(text, BLACK)
+
     # --- Limit to 60 frames per second
-    clock.tick(60)
-     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit()
+        clock.tick(snake_speed)
+        pygame.display.update()
+    # Close the window and quit.
+    # If you forget this line, the program will 'hang'
+    # on exit if running from IDLE.
+    pygame.quit()
+    quit()
+
+gameLoop()
